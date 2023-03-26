c27e5d07cd7d1be98ab240e1b7036a8cea1af90d
tristandantin <tristandantin@gmail.com>
2023-03-26 12:51:19 +0200

    Commit Ex4

--- module.py
+++ module.py
@@ -1,4 +1,5 @@
 import requests
+import pandas as pd
 
 def get_manor_ids(place_id):
     """
@@ -15,8 +16,55 @@ def get_manor_ids(place_id):
         manor_ids = [manor['id'] for manor in place_data['manors']]
     
     return manor_ids
+
+
+def place_ids():
+    """
+    Retrieves a list of all place IDs in Derbyshire.
+    """
+    url = "https://opendomesday.org/api/1.0/county/dby"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        place_ids = [place["id"] for place in data["places_in_county"]]
+        return place_ids
+    else:
+        print(f"Failed to retrieve data. Error code: {response.status_code}")
+
+def get_all_manor_ids_in_derbyshire():
+    """
+    Retrieves a list of all manor IDs in all places in Derbyshire.
+    """
+    all_manor_ids = []
+    cpt=0
+    # Call place_ids() to retrieve a list of place IDs in Derbyshire
+    place_ids_list = place_ids()
+    # Iterate through all place IDs in Derbyshire and call get_manor_ids for each place
+    for place_id in place_ids_list:
+        manor_ids = get_manor_ids(place_id)
+        all_manor_ids.extend(manor_ids)
+    return all_manor_ids
+
+
+
+def get_manor_info(manor_ids):
+    manor_info = []
+    for manor_id in manor_ids:
+        response = requests.get(f"https://opendomesday.org/api/1.0/manor/{manor_id}/")
+        if response.status_code == 200:
+            data = response.json()
+            manor = {
+                "id": manor_id,
+                "geld": data["geld"],
+                "total_ploughs": data["totalploughs"]
+            }
+            manor_info.append(manor)
+    return manor_info
+
+
 if __name__ == '__main__':
-    # Test the get_manor_ids function
-    place_id = 20086
-    manor_ids = get_manor_ids(place_id)
-    print(f"Manor IDs for place {place_id}: {manor_ids}")
+    manor_ids = get_all_manor_ids_in_derbyshire()
+    manor_info = get_manor_info(manor_ids)
+    df = pd.DataFrame(manor_info)
+    print(df)
