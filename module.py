ed763e5c9845f0472a9151979bb15e468a81f7c4
tristandantin <tristandantin@gmail.com>
2023-03-26 13:16:55 +0200

    Final Commit

--- module.py
+++ module.py
@@ -18,37 +18,11 @@ def get_manor_ids(place_id):
     return manor_ids
 
 
-def place_ids():
-    """
-    Retrieves a list of all place IDs in Derbyshire.
-    """
-    url = "https://opendomesday.org/api/1.0/county/dby"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        data = response.json()
-        place_ids = [place["id"] for place in data["places_in_county"]]
-        return place_ids
-    else:
-        print(f"Failed to retrieve data. Error code: {response.status_code}")
-
-def get_all_manor_ids_in_derbyshire():
-    """
-    Retrieves a list of all manor IDs in all places in Derbyshire.
-    """
-    all_manor_ids = []
-    cpt=0
-    # Call place_ids() to retrieve a list of place IDs in Derbyshire
-    place_ids_list = place_ids()
-    # Iterate through all place IDs in Derbyshire and call get_manor_ids for each place
-    for place_id in place_ids_list:
-        manor_ids = get_manor_ids(place_id)
-        all_manor_ids.extend(manor_ids)
-    return all_manor_ids
-
-
 
 def get_manor_info(manor_ids):
+    """
+    Takes a list of manor IDs as a parameter and returns a list of dictionaries containing information about each manor.
+    """
     manor_info = []
     for manor_id in manor_ids:
         response = requests.get(f"https://opendomesday.org/api/1.0/manor/{manor_id}/")
@@ -64,7 +38,7 @@ def get_manor_info(manor_ids):
 
 
 if __name__ == '__main__':
-    manor_ids = get_all_manor_ids_in_derbyshire()
-    manor_info = get_manor_info(manor_ids)
-    df = pd.DataFrame(manor_info)
-    print(df)
+    # Test the get_manor_ids function
+    place_id = 20385
+    manor_ids = get_manor_ids(place_id)
+    print(f"Manor IDs for place {place_id}: {manor_ids}")
--- script.py
+++ script.py
@@ -1,6 +1,17 @@
 import requests
+import pandas as pd
+from module import get_manor_ids
+from module import get_manor_info
 
 def place_id():	
+	"""
+	Retrieves a list of all place IDs in Derbyshire from the OpenDomesday API.
+
+	Returns:
+	--------
+	list of int:
+		A list of all place IDs in Derbyshire.
+	"""
 	url = "https://opendomesday.org/api/1.0/county/dby"
 	response = requests.get(url)	
 	if response.status_code == 200:
@@ -9,3 +20,39 @@ def place_id():
 		return place_ids
 	else:
 		print(f"Failed to retrieve data. Error code: {response.status_code}")
+
+def get_all_manor_ids_in_derbyshire(place_ids_list):
+	"""
+	Retrieves a list of all manor IDs in all places in Derbyshire.
+
+	Parameters:
+	-----------
+	place_ids_list: list of int
+	A list of all place IDs in Derbyshire.
+
+	Returns:
+	--------
+	list of int:
+		A list of all manor IDs in Derbyshire.
+	"""
+	all_manor_ids = []
+	cpt=0
+	# Iterate through all place IDs in Derbyshire and call get_manor_ids for each place
+	for place_id in place_ids_list:
+		manor_ids = get_manor_ids(place_id)
+		all_manor_ids.extend(manor_ids)
+	return all_manor_ids
+
+
+
+if __name__ == '__main__':
+	place_ids=place_id()
+	print(place_ids)
+	manor_ids = get_all_manor_ids_in_derbyshire(place_ids)
+	manor_info = get_manor_info(manor_ids)
+	df = pd.DataFrame(manor_info)
+	print(df)	
+	total_geld_paid = df['geld'].sum()
+	total_ploughs_owned = df['total_ploughs'].sum()
+	print("total_geld_paid = ", total_geld_paid)
+	print("total_ploughs_owned = ", total_ploughs_owned )
